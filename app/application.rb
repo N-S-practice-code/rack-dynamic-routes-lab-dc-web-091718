1f67e70be80ef6828584634b91c36e96df5798e6
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-08 13:58:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,13 @@ class Application
     if req.path.match(/items/)
  
       item_name = req.path.split("/items/").last 
-      item = @@songs.find{|s| s.title == song_title}
+      item = @@item.find{|i| i.name == item_name}
       if item
-        resp.write song.artist
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
